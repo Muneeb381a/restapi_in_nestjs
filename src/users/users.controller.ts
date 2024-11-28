8bdfc1a2c4bb4e853a73e93502ce3d8abadad420
Muneeb381a <muneebafzal381a@gmail.com>
2024-11-29 01:57:26 +0500

    creating controllers for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get() //getting all the users
+    findAll(@Query('role') role?: 'Intern' | 'Enginner' | 'Admin') {
+        return []
+    }
+    @Get(':id') //geeting users through id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+    @Post()  //post route for users
+    create(@Body() user: {}){
+        return user
+    }
+    @Patch(':id') //update users
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate }
+    }
+    @Delete(':id') //delete method for user
+    delete(@Param('id') id: string) {
+        return { id }
+    }
+}
